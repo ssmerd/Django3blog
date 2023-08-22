2b3f03c70a184fc82ec34f3432d81a7a8174e842
Sebastian Smerd <ssmerd@Sebastians-MBP.station>
2023-08-22 16:02:29 +0100

    final deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -34,7 +34,7 @@ DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ["mb2test.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["https://codestar-ssmerd-3446a83800b8.herokuapp.com/", "localhost"]
 
 # Application definition
 
